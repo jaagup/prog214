401ac36cef05409f9ddb2586633d856ab55307a7
haldur <haldur@WS3-3.kolledz>
2014-11-14 17:26:32 +0200

    Tere

--- versioonid/src/versioonid/versioonid.java
+++ versioonid/src/versioonid/versioonid.java
@@ -3,7 +3,7 @@ package versioonid;
 public class versioonid {
 
 	public static void main(String[] args){
-		System.out.println("Tere_Git_uusss");
+		System.out.println("Tere_Git_uussss");
 	}
 	
 }
